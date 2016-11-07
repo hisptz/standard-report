80554ec9d8c7201cff297dbae0479f1550a15397
Vincent P. Minde <vincentminde@gmail.com>
2016-11-07 13:04:10 +0300

    added printing

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -630,6 +630,27 @@ var appControllers = angular.module('appControllers', [])
             $scope.savingComment = "";
             //toaster.pop('info', "Information", "No comments where found.");
         });
+        $scope.printReport = function()
+        {
+            var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+
+            mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+
+            mywindow.document.write('</head><body >');
+            mywindow.document.write('<h1>' + document.title  + '</h1>');
+            mywindow.document.write(document.getElementById("printablereport").innerHTML);
+            mywindow.document.write('</body></html>');
+
+            mywindow.document.close(); // necessary for IE >= 10
+            mywindow.focus(); // necessary for IE >= 10*/
+
+            mywindow.print();
+            mywindow.close();
+
+            return true;
+
+        }
         $scope.saveComment = function () {
             $scope.savingComment = "savingLoad";
             if ($scope.commentData.lastCommenter) {
