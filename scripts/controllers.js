1e159455279d3bb5a244cec83691cc01c275c176
Vincent P. Minde <vincentminde@gmail.com>
2016-11-14 12:09:45 +0300

    Fixed bug with printing

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -12,22 +12,25 @@ function kendoPrint(){
     });
 }
 function browserPrint(){
-    /*var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-
-    mywindow.document.write('<html><head><title>' + document.title  + '</title><style>.noPrint {display: none;}</style>');
-
-    mywindow.document.write('</head><body >');
-    mywindow.document.write('<h1>' + document.title  + '</h1>');
-    mywindow.document.write(document.getElementById("printablereport").innerHTML);
-    mywindow.document.write('</body></html>');
-
-    mywindow.document.close(); // necessary for IE >= 10
-    mywindow.focus(); // necessary for IE >= 10*!/
-
-    mywindow.print();
-    mywindow.close();*/
     window.print();
+    return true;
+}
+function browserPrint2(){
+    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+
+     mywindow.document.write('<html><head><title>' + document.title  + '</title><style>.noPrint {display: none;}</style>');
+
+     mywindow.document.write('</head><body >');
+     mywindow.document.write('<h1>' + document.title  + '</h1>');
+     mywindow.document.write(document.getElementById("printablereport").innerHTML);
+     mywindow.document.write('</body></html>');
+
+     mywindow.document.close(); // necessary for IE >= 10
+     mywindow.focus(); // necessary for IE >= 10*!/
+
+     mywindow.print();
+     mywindow.close();
     return true;
 }
 /* Controllers */
@@ -661,7 +664,7 @@ var appControllers = angular.module('appControllers', [])
         });
         $scope.printReport = function()
         {
-            browserPrint();
+            browserPrint2();
             //kendoPrint();
 
         }
