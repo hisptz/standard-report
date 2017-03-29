113899b8bbe5fd5308722d9c12ba8a2e5cfe8304
Vincent P. Minde <vincentminde@gmail.com>
2017-03-29 16:18:54 +0300

    Fixed issue with excel on hidden table data

--- scripts/services.js
+++ scripts/services.js
@@ -1654,6 +1654,9 @@ var appServices = angular.module('appServices', ['ngResource'])
                 var str = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>';
                 console.log("Title:",tables);
                 tables.each(function(index){
+                    ($(this).find("td.hidden").each(function(index2){
+                        this.remove();
+                    }));
                     ctx["table" + index] = this.innerHTML;
                     if(this.title == "no-border"){
                         str += '<table>{' + "table" + index+'}</table><br />';
