292a8b09a86e6e34712af7fd234814681ed7fe2a
Vincent P. Minde <vincentminde@gmail.com>
2017-06-28 13:49:23 +0300

    Fixed some comma issue

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1810,6 +1810,7 @@ var appControllers = angular.module('appControllers', [])
                         $.each($('td'), function () {
                             if(!isNaN($(this).text().split(",").join("")))
                             {
+                                $(this).text($(this).text().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
                                 $(this).css('text-align', 'right');
                             }
                         });
