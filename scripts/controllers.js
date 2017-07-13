d6d90a2c41c5dd8322cbaab26c8f008a82d86124
kelvin mbwilo <kelvinmbwilo@gmail.com>
2017-07-13 17:51:33 +0300

    move numbers to the right

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1814,10 +1814,11 @@ var appControllers = angular.module('appControllers', [])
                             $scope.progressValue = 100;
                             $scope.loadingReport = false;
                             $.each($('td'), function () {
-                                $(this).addClass("push-right");
+                                $(this).setAttribute("class", "push-right");
                                 if(!isNaN($(this).text().split(",").join("")))
                                 {
                                     console.log("some text", $(this).text())
+                                    console.log("some text", $(this).addAtt())
                                     // $(this).addClass("push-right");
                                     $(this).text($(this).text().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
                                     $(this).css('text-align', 'right');
