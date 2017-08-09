cea203a05bfe5592f2dcb5b6f5ad48a8dff586e8
Vincent P. Minde <vincentminde@gmail.com>
2017-08-09 11:18:15 +0300

    Fixed excel bug

--- scripts/services.js
+++ scripts/services.js
@@ -1746,7 +1746,7 @@ var appServices = angular.module('appServices', ['ngResource'])
             }
         };
     })
-    .factory('Excel1', function ($window) {
+    .factory('Excel', function ($window) {
         var uri = 'data:application/vnd.ms-excel;base64,',
             template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table border="1">{table}</table><br /><table border="1">{table}</table></body></html>',
             base64 = function (s) {
