94d1fe9c4812ddd20349c9475e02a8cc51892217
Vincent P. Minde <vincentminde@gmail.com>
2018-06-01 08:09:24 +0300

    Fixed printing data point

--- scripts/directives.js
+++ scripts/directives.js
@@ -1,4 +1,12 @@
 'use strict';
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(search, this_len) {
+        if (this_len === undefined || this_len > this.length) {
+            this_len = this.length;
+        }
+        return this.substring(this_len - search.length, this_len) === search;
+    };
+}
 HTMLCollection.prototype.sort = function (callback) {
     //this.slice(callback);
     var items = Array.prototype.slice.call(this);
