94e776be91a2228d452bd4293d533a7ff3a18d47
Andre <amgowano.apm@gmail.com>
2019-03-28 21:46:24 +0300

    fix(services.js): handle display none fields on excel downloads

--- scripts/services.js
+++ scripts/services.js
@@ -2087,6 +2087,7 @@ var appServices = angular
                   ); //.replace(/'/g, '&apos;'));
                 }
                 if ($(this).css("display") == "none") {
+                  this.remove();
                   $(this).empty();
                 }
               });
